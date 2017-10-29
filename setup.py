160a9d0c668286127f050c5f3a3aa9ebcc3f3bfa
Fedor Borshev <f@f213.in>
2017-10-29 13:40:54 +0300

    First commit

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='scrapyd-client',
+    version='0.0.1',
+    description="Simple client to scrapyd. Done right.",
+    keywords=[],
+    url="https://github.com/f213/scrapyd-client/",
+    author="Fedor Borshev",
+    author_email="f@f213.in",
+    license="MIT",
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+    ],
+    include_package_data=True,
+    zip_safe=False,
+)
