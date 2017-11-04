c521d21941d1ab178dd1d6c6e3d7ae4ff7094fd3
Fedor Borshev <f@f213.in>
2017-11-04 21:46:54 +0300

    Version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='scrapyc',
-    version='0.0.2',
+    version='0.0.4',
     description="Simple client to scrapyd. Done right.",
     keywords=[],
     url="https://github.com/f213/scrapyd-client/",
